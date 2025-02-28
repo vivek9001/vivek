7d976b8245d31229c46f0a6dc485ee664fa47294
vivek9001 <vip99455@gmail.com>
2025-02-28 09:59:46 +0530

    Add files via upload

---
+++ AccessedIfArray.java
@@ -0,0 +1,9 @@
+class AccessedIfArray {
+    public static void main(String[] args) {
+        int[] numbers = {11, 22, 33, 44, 55, 66, 77, 88, 99, 100}; 
+
+        for (int i = 0; i < numbers.length; i++) { 
+            System.out.println("index of element " + i + " & element is = " + numbers[i]);
+        }
+    }
+}
